6277efc5ecf3bd7dd3b2efc9289a69029df00565
stevezheng <stevezheng1989@gmail.com>
2015-07-17 07:00:23 +0800

    新增 删除地址

--- www/js/address/address.manage.js
+++ www/js/address/address.manage.js
@@ -5,11 +5,13 @@
     .module('address.manage', [])
     .controller('AddressManageCtrl', AddressManageCtrl);
 
-  AddressManageCtrl.$inject = ['$scope'];
+  AddressManageCtrl.$inject = ['$scope', '$yikeUtils'];
 
   /* @ngInject */
-  function AddressManageCtrl($scope) {
+  function AddressManageCtrl($scope, $yikeUtils) {
     $scope.init = init;
+    $scope.edit = edit;
+    $scope.del = del;
 
     init();
 
@@ -26,6 +28,35 @@
         .then(function(addresses) {
           $scope.addresses = addresses;
         })
+    }
+
+    function del(address) {
+      $yikeUtils
+        .confirm('提示', '是否删除地址?')
+        .then(function(res) {
+          if (res) {
+            D('Address')
+              .where({userId: AV.User.current().id, objectId: address.id})
+              .find()
+              .then(function(_address) {
+                _address.destroy({
+                  success: function() {
+                    $yikeUtils.alert('提示', '删除成功');
+                    init();
+                  },
+
+                  error: function() {
+                    $yikeUtils.alert('提示', '删除失败');
+                    init();
+                  }
+                });
+              })
+          }
+        })
+    }
+
+    function edit(address) {
+
     }
   }
 })();
