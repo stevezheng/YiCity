130fd8ce8d8f8201c7c28c8cf883443671351fcf
stevezheng <stevezheng1989@gmail.com>
2015-08-02 15:08:26 +0800

    add list-refresh

--- www/js/shop/shop.factory.js
+++ www/js/shop/shop.factory.js
@@ -2,17 +2,36 @@
   'use strict';
 
   angular
-    .module('shop.factory', [])
-    .factory('Shop', Shop);
+    .module('shop.factory', ['ionic'])
+    .factory('Shop', Factory);
 
-  Shop.$inject = [];
+  Factory.$inject = ['$ionicLoading'];
 
   /* @ngInject */
-  function Shop() {
-    return {
-    };
+  function Factory($ionicLoading) {
+    var Shop = AV.Object.extend('Shop', {
+    }, {
+      getCount: function() {
+        var query = new AV.Query(S);
+        return query.count();
+      },
+      getFeeds: function(condition, page, row) {
+        $ionicLoading.show({
+          template: '载入中...'
+        });
 
-    ////////////////
+        var query = new AV.Query(Shop);
+        query.skip(page * row);
+        query.limit(row);
+        var collection = query.collection();
 
+        return collection.fetch().then(function(res) {
+          $ionicLoading.hide();
+          return res;
+        });
+      }
+    });
+
+    return Shop;
   }
 })();
